6d39f2fda5d38b9ae9a1c8e785ca322f21e8a8f4
rd2016 <rd2016@ic.ac.uk>
2020-02-17 15:38:17 +0000

    first set of anonymized results; unconvincing accuracy

--- code/auto_tune.py
+++ code/auto_tune.py
@@ -12,25 +12,28 @@ import autosklearn.classification
 def auto_tune(file="anon_data/adult-1hot.csv", y_name="salary"):
     data = pd.read_csv(file)
     y = data[y_name]
-    X = data.drop(columns=[y_name], axis=1)
+    X = data.drop(columns=[y_name, "index"], axis=1)
+    X = pd.get_dummies(X)
+
     print(X)
     print(y)
+    #feature_types = ['Categorical']*len(X.columns)
 
     X_train, X_test, y_train, y_test = \
         sklearn.model_selection.train_test_split(X, y, random_state=1)
-
+    print(X_test.shape, X_train.shape)
     automl = autosklearn.classification.AutoSklearnClassifier(
-        time_left_for_this_task=12*60*60,
+        time_left_for_this_task=10*60*60,
         per_run_time_limit=60*60,
-        tmp_folder='/tmp/autosklearn_parallel_1_example_tmp',
-        output_folder='/tmp/autosklearn_parallel_1_example_out',
+        tmp_folder='/tmp/autosklearn_adult_tmp',
+        output_folder='/tmp/autosklearn_adult_out',
         disable_evaluator_output=False,
         n_jobs=4,
-        seed=5,
+        seed=1,
         delete_output_folder_after_terminate=True,
         delete_tmp_folder_after_terminate=True,
     )
-    automl.fit(X_train, y_train, dataset_name='adult_census')
+    automl.fit(X_train, y_train, dataset_name=f"adult")
     print("\n\n\n\n\n######################################")
     # Print the final ensemble constructed by auto-sklearn.
     print(automl.show_models())
