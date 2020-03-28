733c01af2859bfd2fafc755b52b138e21a58a26f
rd2016 <rd2016@ic.ac.uk>
2020-03-28 17:29:34 +0000

    anonymized mondrian for birth control dataset

--- code/toolbox_linux64/batch_mondrian.sh
+++ code/toolbox_linux64/batch_mondrian.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for k in {100..7350..250}
+for k in {2..20..2} {35..725..15} 1887
 do
     mv configs/mondrian${k}.xml config.xml
-    ./anonymization.sh
     echo "${k} anonymization"
+    ./anonymization.sh
     mv config.xml configs/mondrian${k}.xml
 done
--- code/toolbox_linux64/configs/config_creator_birth.py
+++ code/toolbox_linux64/configs/config_creator_birth.py
@@ -11,39 +11,39 @@ def config_creator(k):
 
         <qid>
             <att index='0' name ='age'>
-                <vgh value='[16:49]'>
+                <vgh value='[16.0:49.0]'>
                 </vgh>
             </att>
             <att index='1' name ='wife_ed'>
-                <vgh value='[1:4]'>
+                <vgh value='[1.0:4.0]'>
                 </vgh>
             </att>
             <att index='2' name ='husb_ed'>
-                <vgh value='[1:4]'>
+                <vgh value='[1.0:4.0]'>
                 </vgh>
             </att>
             <att index='3' name ='no_kids'>
-                <vgh value='[0:16]'>
+                <vgh value='[0.0:16.0]'>
                 </vgh>
             </att>
             <att index='4' name ='wife_rel'>
-                <vgh value='[0:1]'>
+                <vgh value='[0.0:1.0]'>
                 </vgh>
             </att>
             <att index='5' name ='wife_works'>
-                <vgh value='[0:1]'>
+                <vgh value='[0.0:1.0]'>
                 </vgh>
             </att>
             <att index='6' name ='husb_occupation'>
-                <vgh value='[1:4]'>
+                <vgh value='[1.0:4.0]'>
                 </vgh>
             </att>
             <att index='7' name ='SOL_index'>
-                <vgh value='[1:4]'>
+                <vgh value='[1.0:4.0]'>
                 </vgh>
             </att>
             <att index='8' name ='media_exp'>
-                <vgh value='[0:1]'>
+                <vgh value='[0.0:1.0]'>
                 </vgh>
             </att>
         </qid>
@@ -57,5 +57,5 @@ def config_creator(k):
     f.write(config)
     f.close()
 
-for k in list(range(2,20,2)) + list(range(2, 736,15)) + [1887]:
+for k in list(range(2,21,2)) + list(range(35, 736,15)) + [1887]:
     config_creator(k)
