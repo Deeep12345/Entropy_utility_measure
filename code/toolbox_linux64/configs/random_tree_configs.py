22b643acd150d86cba2f0517ec4c4dcb454c3d31
Romain <rdespoelberch@yahoo.com>
2020-04-10 13:29:30 +0200

    trying mondrian categorization

--- code/toolbox_linux64/configs/random_tree_configs.py
+++ code/toolbox_linux64/configs/random_tree_configs.py
@@ -96,5 +96,60 @@ def make_datafly_config(name):
 
     return full_xml
 
-for i in range(1,201):
-    make_datafly_config(i)
+
+def make_mondrian_config(name):
+    mappings = {}
+    full_xml = """
+    <config method='Mondrian' k='2'>
+    <input filename='../../datasets/birth_control/cmc_ups_4cat.csv' separator=','/>
+     <!-- If left blank, separator will be set as comma by default.-->"""
+    full_xml += f"<output filename='../anon_data/birth_random_groups/mondrian{name}.csv' format ='genVals'/>"
+    full_xml += """<!-- Format options = {genVals, genValsDist, anatomy}. If left blank,
+    output format will be set as genVals by default.-->
+    <id> <!-- List of identifier attributes, if any, these will be excluded from the output -->
+    </id>
+    <qid>"""
+
+    for i, attr in enumerate(data.columns[:-1]):
+        full_xml += (f"<att index='{i}' name='{attr}'>")
+
+        if choices[attr] == 2:
+            full_xml += ("<vgh value='[0:1]'/>")
+        else:
+            full_xml += ("<vgh value='[1:4]'/>")
+            ord = [1,2,3,4]
+            random.shuffle(ord)
+            full_xml += "<map>"
+            for i, m in enumerate(ord):
+                full_xml += f"<entry cat='{i+1}' int='{m}'/>"
+            full_xml += "</map>"
+            #print(attr, selected_t[0])
+            #print(trees_4[selected_t[0]]["xml_tree"])
+            mappings[attr] = ord
+
+        full_xml += (f"</att>")
+
+    full_xml += ("</qid>")
+
+    full_xml += ("""
+    <sens>
+    <att index='9' name='class'/>
+    </sens>""")
+
+    # full_xml += "<mapping>"
+    # for attr, mapp in mappings.items():
+    #     full_xml += f"<att name='{attr}'>"
+    #     for i, m in enumerate(mapp):
+    #         full_xml += f"<node used='{m}' mapbackto='{orig}'/>"
+    #     full_xml = full_xml + f"</att>"
+    # full_xml = full_xml + "</mapping>"
+    full_xml = full_xml + "</config>"
+    xml_tree = bs(full_xml, 'xml')
+    f = open(f"mondrian{name}.xml", "w+")
+    f.write(xml_tree.prettify())
+    f.close()
+
+    return xml_tree
+
+#for i in range(1,201):
+print(make_mondrian_config(1).prettify())
