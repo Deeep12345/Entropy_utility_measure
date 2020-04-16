92b2934724db924416b7d106c09c7401f41255cd
Romain <rdespoelberch@yahoo.com>
2020-04-16 21:50:49 +0200

    implemented precision for birth randoms

--- code/results/birth_randoms/analysis.py
+++ code/results/birth_randoms/analysis.py
@@ -5,17 +5,24 @@ from diameter_metric import diam
 from classification_metric import class_metric
 from RFCs_train import train_test
 from entropy import cond_entr
+from hierarchy_metrics import precision
 
-def load_csv(algo, no, orig=False):
-    if orig:
+def load_csv(algo, no, original_oh=False, bounded=False):
+    if original_oh:
         data = pd.read_csv(f'../../anon_data/birth_random_groups/original_oh.csv')
+    elif bounded:
+        cols = ["age","wife_ed","husb_ed","no_kids","wife_rel","wife_works",
+            "husb_occupation","SOL_index","media_exp","class"]
+        data = pd.read_csv(f"../../anon_data/birth_random_groups/{algo}{no}.csv",
+            names=cols)
     else:
         data = pd.read_csv(f'../../anon_data/birth_random_groups/{algo}{no}_oh.csv')
-    data.drop("Unnamed: 0", axis=1, inplace=True)
+    if not bounded:
+        data.drop("Unnamed: 0", axis=1, inplace=True)
     return data
 
 
-orig_data = load_csv("", 0, orig=True)
+orig_data = load_csv("", 0, original_oh=True)
 full_anon = orig_data.copy()
 full_anon.replace(0, 1, inplace=True)
 
@@ -23,14 +30,17 @@ max_H = cond_entr(orig_data, full_anon)
 
 results = {}
 for no in range(1,201):
-    for algo in ["datafly", "mondrian"]:
+    for algo in ["mondrian", "datafly"]:
         r = {}
         anon_data = load_csv(algo, no)
+        bounded_data = load_csv(algo, no, bounded=True)
 
+        r["precision"] = precision(bounded_data, algo, no)
         r["dm"] = diam(anon_data)
         r["cm"] = class_metric(anon_data)
         r["entropy"] = cond_entr(orig_data, anon_data)/max_H
         r["acc"] = train_test(orig_data, anon_data)
+
 
         print(no, algo, r)
         results[(algo, no)] = r
---
+++ code/results/birth_randoms/hierarchy_metrics.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import xml.etree.ElementTree as et
+import xmltodict as xdict
+import re
+
+def parse_range(x):
+    if ":" not in x:
+        x = x.split(".")
+        val = int(x[0][1:])
+        return val, val
+
+    vals = re.split(':', x[1:-1])
+    vals[0] = vals[0].split(".")[0]
+    vals[1] = vals[1].split(".")[0]
+    low = int(vals[0]) if x[0] == '[' else int(vals[0]) + 1
+    high = int(vals[1]) if x[-1] == ']' else int(vals[1]) - 1
+    return low, high
+
+
+def load_config(algo, no):
+    xml = et.parse(f"../../toolbox_linux64/configs/random_configs/{algo}{no}.xml")
+    root = xml.getroot()
+    return root
+
+def get_datafly_trees(root):
+    trees = {}
+    for child in root[3]:
+        name = child.attrib["name"]
+        v = child[0]
+        v = et.tostring(v)
+        t = xdict.parse(v)
+        t = dict(t["vgh"])
+        trees[name] = t
+    return trees
+
+def get_datafly_depths(trees):
+    depths = {}
+    for name, t in trees.items():
+        d = {}
+        if "node" in t:
+            d[t["@value"]] = 2
+            for node in t["node"]:
+                d[node["@value"]] = 1
+        else:
+            d[t["@value"]] = 1
+        depths[name] = d
+    return depths
+
+def get_mondrian_depths(bounded_data):
+    depths = {}
+    for col in bounded_data.columns[:-1]:
+        vals = set(bounded_data[col])
+        ds = {}
+        for v in vals:
+            lo, hi = parse_range(v)
+            ds[v] = hi - lo
+        depths[col] = ds
+    return depths
+
+
+def precision(bounded_data, algo, no):
+    root = load_config(algo, no)
+    if algo == "datafly":
+        trees = get_datafly_trees(root)
+        depths = get_datafly_depths(trees)
+        max_depths = {attr:max(depths[attr].values()) for attr in depths}
+        print(max_depths)
+    elif algo == "mondrian":
+        depths =get_mondrian_depths(bounded_data)
+        max_depths = {attr:(1 if attr in ["wife_works", "wife_rel", "media_exp"]
+                            else 3) for attr in depths}
+        print(max_depths)
+
+    prec = 0
+
+    for c in bounded_data.columns[:-1]:
+        counts = bounded_data[c].value_counts()
+        dist = 0
+        for count in counts.index:
+            if '.' not in count:
+                dist += counts[count] * depths[c][count] / max_depths[c]
+        prec += dist
+
+    prec /= (len(bounded_data) * len(bounded_data.columns[:-1]))
+    return 1 - prec
