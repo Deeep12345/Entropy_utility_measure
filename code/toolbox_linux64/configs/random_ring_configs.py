8747f5bc2b879d9b2b4438bdb6a67ce0bd2cb071
Romain <rdespoelberch@yahoo.com>
2020-04-24 15:56:37 +0200

    can make shuffled/unshuffled random trees in datafly

--- code/toolbox_linux64/configs/random_ring_configs.py
+++ code/toolbox_linux64/configs/random_ring_configs.py
@@ -28,113 +28,112 @@ def categorize(data, no_bins):
     return data
 
 
-#
-# values = {}
-# for col in data.columns[:-1]:
-#     values[col] = set(data[col])
-#     print(f"{col}..... {values[col]}")
-#
-# ps = list(mit.set_partitions([1,2,3,4], 2))
-# ps.extend(list(mit.set_partitions([1,2,3,4], 3)))
-# ps.extend(list(mit.set_partitions([1,2,3,4], 4)))
-#
-# choices = {c:len(set(values[c])) for c in data.columns[:-1]}
-#
-# def create_4tree():
-#     x = list(range(1,5))
-#     res = "<vgh value='[1:4]'>\n"
-#
-#     part = random.choice(ps)
-#     fp = flatten(part)
-#
-#     mapping = {}
-#     for j, m in enumerate(fp):
-#         mapping[m] = j+1
-#     mapped_p = [list(map(lambda x: mapping[x], l)) for l in part]
-#
-#     for p in mapped_p:
-#         res += f"\t<node value='[{min(p)}:{max(p)}]'/>\n"
-#
-#     res += "</vgh>"
-#     return res, mapping
-#
-#
-# def make_datafly_config(name):
-#     mappings = {}
-#     full_xml = """
-#     <config method='Datafly' k='2'>
-#     <input filename='../../datasets/birth_control/cmc_ups_4cat.csv' separator=','/>
-#      <!-- If left blank, separator will be set as comma by default.-->"""
-#     full_xml += f"<output filename='../anon_data/birth_random_groups/datafly{name}.csv' format ='genVals'/>"
-#     full_xml+= """<!-- Format options = {genVals, genValsDist, anatomy}. If left blank,
-#     output format will be set as genVals by default.-->
-#     <id> <!-- List of identifier attributes, if any, these will be excluded from the output -->
-#     </id>
-#     <qid>"""
-#
-#     for i, attr in enumerate(data.columns[:-1]):
-#         full_xml = full_xml + (f"<att index='{i}' name='{attr}'>")
-#
-#         if choices[attr] == 2:
-#             full_xml = full_xml + ("<vgh value='[0:1]'/>")
-#         else:
-#             tree, mapping = create_4tree()
-#             #print(attr, selected_t[0])
-#             #print(trees_4[selected_t[0]]["xml_tree"])
-#             mappings[attr] = mapping
-#             full_xml = full_xml + tree
-#
-#         full_xml = full_xml + (f"</att>")
-#
-#     full_xml = full_xml + ("</qid>")
-#
-#     full_xml = full_xml + ("""
-#     <sens>
-#     <att index='9' name='class'/>
-#     </sens>""")
-#
-#     full_xml = full_xml + "<mapping>"
-#     for attr, mapp in mappings.items():
-#         full_xml = full_xml + f"<att name='{attr}'>"
-#         for orig, m in mapp.items():
-#             full_xml = full_xml + f"<node used='{m}' mapbackto='{orig}'/>"
-#         full_xml = full_xml + f"</att>"
-#     full_xml = full_xml + "</mapping>"
-#     full_xml = full_xml + "</config>"
-#     xml_tree = bs(full_xml, 'xml')
-#     f = open(f"datafly{name}.xml", "w+")
-#     f.write(xml_tree.prettify())
-#     f.close()
-#
-#     return full_xml
-#
-#
+def cut(l, i):
+    return [l[:i], l[i:]]
+
+
+def random_partition(l):
+    bins = len(l)
+    p = [[] for i in range(5)]
+    for i in l:
+        p[np.random.randint(0,len(p))].append(i)
+    p = list(filter(lambda x: x, p))
+    return p
+
+
+def ordered_partition(l):
+    len_splits = []
+    p = random_partition(l)
+    s_sizes = [len(part) for part in p]
+    o_p = [ l[x-y:x] for x, y in zip(itertools.accumulate(s_sizes), s_sizes)]
+    return o_p
+
+
+def make_tree(l):
+    tree = {}
+    l = ordered_partition(l)
+    lens = [len(item) for item in l]
+    cur_val = (min(l[0]), max(l[len(l)-1]))
+    tree["value"] = cur_val
+    if min(lens) > 1:
+        cs = []
+        for subset in l:
+            cs.append(make_tree(subset))
+            tree["children"] = cs
+    else:
+        minmaxs = [{"value":(min(item), max(item))} for item in l]
+        tree["children"] = minmaxs
+    return tree
+
+
+def min_depth(t):
+    mi, ma = t["value"]
+    if mi == ma or "children" not in t:
+        return 1
+    else:
+        return min([1 + min_depth(ch) for ch in t["children"]])
+
+
+def flatten_recur(t, d, max_d):
+    if d == max_d:
+        if "children" in t:
+            del(t["children"])
+    else:
+        for child in t["children"]:
+            flatten_recur(child, d+1, max_d)
+
+
+def flatten_tree(t):
+    min_d = min_depth(t)
+    flatten_recur(t, 1, min_d)
+
+
+def make_vgh(no_bins):
+    l = list(range(no_bins))
+    t = make_tree(l)
+    flatten_tree(t)
+    return t
+
+
+def tree_to_xml(t, head=False):
+    mi, ma = t["value"]
+    res = ""
+    title = "vgh" if head else "node"
+    if 'children' not in t:
+        res += (f"<{title} value='[{mi}:{ma}]'/>")
+    else:
+        res += (f"<{title} value='[{mi}:{ma}]'>")
+        for c in t["children"]:
+            res += tree_to_xml(c)
+        res += (f"</{title}>")
+    return res
+
+
 def make_mondrian_config(name, k, no_cols, no_bins):
     mappings = {}
     full_xml = f"""
     <config method='Mondrian' k='{k}'>
-    <input filename='../../datasets/ring/ring_cat.csv' separator=','/>"""
-    full_xml += f"<output filename='../anon_data/ring_randoms/mondrian{name}.csv' format ='genVals'/>"
-    full_xml += """<id></id><qid>"""
+    <input filename='../../datasets/ring/ring_cat.csv' separator=','/>
+    <output filename='../anon_data/ring_randoms/mondrian{name}.csv' format ='genVals'/>
+    <id></id>
+    <qid>"""
 
     for i in range(0,no_cols):
-        full_xml += f"<att index='{i}' name='{i}th'>"
-        full_xml += f"<vgh value='[0:{no_bins-1}]'/>"
+        full_xml += f"""
+        <att index='{i}' name='{i}th'>
+        <vgh value='[0:{no_bins-1}]'/>"""
 
         ord = list(range(0,no_bins))
         random.shuffle(ord)
         full_xml += "<map>"
         for j, m in enumerate(ord):
             full_xml += f"<entry cat='{j}' int='{m}'/>"
-        full_xml += "</map>"
-        full_xml += f"</att>"
+        full_xml += "</map></att>"
 
-    full_xml += "</qid>"
     full_xml += f"""
-    <sens>
-    <att index='{no_cols}' name='class'/>
-    </sens>"""
-    full_xml = full_xml + "</config>"
+    </qid>
+    <sens><att index='{no_cols}' name='class'/></sens>
+    </config>"""
     xml_tree = bs(full_xml, 'xml')
     f = open(f"ring_randoms/mondrian{name}.xml", "w+")
     f.write(xml_tree.prettify())
@@ -142,10 +141,54 @@ def make_mondrian_config(name, k, no_cols, no_bins):
 
     return xml_tree
 
+
+def make_datafly_config(name, k, no_cols, no_bins, shuffled=False):
+    full_xml = f"""
+    <config method='Datafly' k='{k}'>
+    <input filename='../../datasets/ring/ring_cat.csv' separator=','/>
+    <output filename='../anon_data/ring_randoms/datafly{name}.csv' format ='genVals'/>
+    <id></id>
+    <qid>"""
+
+    for i in range(no_cols):
+        full_xml += (f"<att index='{i}' name='{i}th'>")
+        #VGH
+        tree = make_vgh(no_bins)
+        tree = tree_to_xml(tree, head=True)
+        full_xml += tree
+
+        #Mapping
+        if shuffled:
+            ord = list(range(no_bins))
+            random.shuffle(ord)
+            full_xml += "<map>"
+            for j, m in enumerate(ord):
+                full_xml += f"<entry cat='{j}' int='{m}'/>"
+            full_xml += "</map>"
+        full_xml += "</att>"
+
+    full_xml += """
+    </qid>
+    <sens>
+    <att index='9' name='class'/>
+    </sens>
+    </config>"""
+
+    xml_tree = bs(full_xml, 'xml')
+    is_shuffled = "_shuffled" if shuffled else ""
+    f = open(f"ring_randoms/datafly{name}{is_shuffled}.xml", "w+")
+    f.write(xml_tree.prettify())
+    f.close()
+
+    return full_xml
+
+
+no_bins = 20
+no_cols = 20
 ks = np.random.poisson(1, 200)
 ks = ks.astype(int) + 2
-print(pd.Series(ks).value_counts())
+print(pd.Series(ks).value_counts(), "\n")
+
 for i, k in enumerate(ks):
-    no_bins = 20
-    no_cols = 20
-    conf = make_mondrian_config(i+1, k, no_cols, no_bins).prettify()
+    make_datafly_config(i+1, k, no_cols, no_bins, shuffled=False)
+    make_datafly_config(i+1, k, no_cols, no_bins, shuffled=True)
