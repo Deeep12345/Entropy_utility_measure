5f7a5ebe8a3a19fedb5019183f7f7fae8f2ce03c
rd2016 <rd2016@ic.ac.uk>
2020-04-30 10:43:05 +0100

    standardised group anon

--- code/toolbox_linux64/batch_mondrian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for k in {2..20..2} {35..725..15} 1887
-do
-    mv configs/mondrian${k}.xml config.xml
-    echo "${k} anonymization"
-    ./anonymization.sh
-    mv config.xml configs/mondrian${k}.xml
-done
