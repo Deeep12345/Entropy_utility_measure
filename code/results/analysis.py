46ca65c58ea05f9c2f29a4d8c283a960d046d6df
rd2016 <rd2016@ic.ac.uk>
2020-05-14 09:43:21 +0100

    adding the updated analysis and autotune, got metrics gor adult datafly

--- code/results/analysis.py
+++ code/results/analysis.py
@@ -49,7 +49,7 @@ full_anon.replace(0, 1, inplace=True)
 max_H = cond_entr_metric(orig_data, full_anon, QIs)
 
 results = {}
-for no in tqdm(range(1, config["no_instances"]+1)):
+for no in tqdm([100, 101] + list(range(103, config["no_instances"]+1))):
 
     for algo in ["datafly"]:
         r = {}
@@ -75,10 +75,10 @@ for no in tqdm(range(1, config["no_instances"]+1)):
 
     if no % 20 == 0:
         df = pd.DataFrame.from_dict(results, orient='index')
-        df.to_csv(f"{config['analysis_name']}/metrics_dat.csv",
+        df.to_csv(f"{config['analysis_name']}/metrics_dat2.csv",
                     index_label=["algo","no"])
 
 print(results)
 df = pd.DataFrame.from_dict(results, orient='index')
-df.to_csv(f"{config['analysis_name']}/metrics_dat.csv",
+df.to_csv(f"{config['analysis_name']}/metrics_dat2.csv",
             index_label=["algo","no"])
--- code/results/auto_tune.py
+++ code/results/auto_tune.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import sys
 from sklearn.model_selection import train_test_split
+import sklearn.metrics
 import autosklearn.regression
+import pickle
 
 
 def auto_tune(file, y_name):
@@ -10,7 +12,7 @@ def auto_tune(file, y_name):
 
     print("Preparing train and test split...")
     y = data[y_name]
-    X = data.drop([y_name, "algo", "no"], axis=1)
+    X = data.drop([y_name, "algo", "no", "acc", "auroc"], axis=1)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
     print(y)
@@ -20,22 +22,27 @@ def auto_tune(file, y_name):
     print(f"y shapes: {y_test.shape, y_train.shape}")
 
     print(f"Starting training on {file}...\n")
+
     automl = autosklearn.regression.AutoSklearnRegressor(
-        time_left_for_this_task=2*60*60,
+        time_left_for_this_task=1*60*60,
+        per_run_time_limit=60*60,
         disable_evaluator_output=False,
         initial_configurations_via_metalearning=False,
-        n_jobs=1
+        n_jobs=6,
+        tmp_folder="/vol/bitbucket/rd2016/tmp",
+        output_folder="/vol/bitbucket/rd2016/out"
     )
-    automl.fit(X_train, y_train)
+    automl.fit(X_train, y_train, X_test=X_test, y_test=y_test)
     print("############ Done ! ################\n")
 
     ensemble = automl.get_models_with_weights()
     print(automl.show_models())
     print(automl.sprint_statistics())
     predictions = automl.predict(X_test)
-    print("Accuracy score", sklearn.metrics.accuracy_score(y_test, predictions))
-    with open(f'automl_{file}.pkl', 'w+') as output:
-        pickle.dump(automl,output)
+    fn = f'automl_{file}.pkl'
+    pickle.dump(automl,fn)
+    print("distances", open(fn, "wb"))
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
