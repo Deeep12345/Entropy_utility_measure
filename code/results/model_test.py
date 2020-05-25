3697928b5fd11c6cf84dbed07972587ce7197c8e
Romain <rdespoelberch@yahoo.com>
2020-05-25 12:39:45 +0200

    reran missing ringrandom metrics, and created model test

--- code/results/model_test.py
+++ code/results/model_test.py
@@ -5,27 +5,41 @@ import autosklearn
 import matplotlib.pyplot as plt
 import sys
 
-fn = sys.argv[1]
+dataset = sys.argv[1]
+target = sys.argv[2]
 
-model = pickle.load(open(f"automl_{fn}.pkl", "rb"))
+model = pickle.load(open(f"autosklearn_models/automl_{dataset}_{target}.pkl", "rb"))
 print(model.get_models_with_weights())
 
 
 #until new model, remove new metrics
-testset = pd.read_csv(f"{fn}/testset.csv")
-X_test = testset.drop(["lr_acc", "algo", "no", "auroc"], axis=1)
-y_test = testset["lr_acc"]
+X_test = pd.read_csv(f"{dataset}/metrics_testset.csv")
+X_test = X_test.drop(["lr_acc", "algo", "no", "auroc"], axis=1)
 
-print(X_test, y_test)
+y_test = pd.read_csv(f"{dataset}/accuracies_testset.csv")
+y_test = y_test[target]
+
 print(X_test.shape, y_test.shape)
 
 predicted = model.predict(X_test)
-print("##### Predictions ######\n", predicted)
-abs_dist = np.abs(predicted - y_test)
-print("##### abs(y_test - predicted) ######\n", abs_dist)
 
-plt.hist(abs_dist, label="abs_dist")
-plt.title("Absolute distance between accuracy predictions and true values")
-plt.xlabel("|y_true - y_pred|")
-plt.ylabel("count")
-plt.show()
+correct_counter, test_counter = 0, 0
+
+for i, x in enumerate(y_test[:-1]):
+    for j, y in enumerate(y_test[i:]):
+        if j != 0:
+            print(f"Comparing test dataset {i} to test dataset {j+i}")
+            print(f"\tpredicted {target} for {i}: {predicted[i]}")
+            print(f"\tpredicted {target} for {j+i}: {predicted[j+i]}")
+            predicted_larger = predicted[i] >= predicted[j+i]
+            print(f"\t{i} predicted more useful than {j+i}? {predicted_larger}")
+            print(f"\tTrue {target} for {i}: {y_test[i]}")
+            print(f"\tTrue {target} for {j+i}: {y_test[j+i]}")
+            larger = y_test[i] >= y_test[j+i]
+            print(f"\t{i} truly more useful than {j+i}? {larger}")
+            if larger == predicted_larger:
+                print("metrics work!")
+                correct_counter+=1
+            test_counter+=1
+
+print(f"The super metric correctly predicted better datasets {correct_counter/test_counter} percent of the time")
