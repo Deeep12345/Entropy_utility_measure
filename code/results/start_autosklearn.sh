109efb0ecc40254a22ec89a45b0369f9a8690626
Romain <rdespoelberch@yahoo.com>
2020-05-28 22:58:15 +0200

    task starting script to start tomorrow morning

--- code/results/start_autosklearn.sh
+++ code/results/start_autosklearn.sh
@@ -1,12 +1,16 @@
 
-
+echo "$1 algo"
 count=10
 for target in "lr_acc" "lr_auroc" "knn_pca_acc" "knn_pca_auroc" "rf_pca_acc" "rf_pca_auroc"
 do
   for dataset in "birth_randoms" "ring_randoms" "adult_randoms" "heart_randoms"
   do
-    echo $count
-    ssh rd2016@corona${count}.doc.ic.ac.uk "cd Entropy_utility_measure/code/results; nohup python3 auto_tune.py ${dataset} ${target} > ${dataset}_${target}.out & "
+    if (($count == 21));
+    then
+      count=$((count+1))
+    fi
+    echo "$(printf %02d $count)corona"
+    ssh rd2016@corona$(printf %02d $count).doc.ic.ac.uk -t "htop" #"cd Entropy_utility_measure/code/results; nohup python3 auto_tune.py ${dataset} ${target} $1> ${dataset}_${target}_$1.out & "
     count=$((count+1))
   done
 done
